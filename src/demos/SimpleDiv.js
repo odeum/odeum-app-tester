7727384a1e520b18af3a33c2dab310677a72461e
Christian Broberg <cb@webhouse.dk>
2017-11-13 11:24:58 +0100

    Playing with content

--- src/demos/SimpleDiv.js
+++ src/demos/SimpleDiv.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Gravatar from 'react-gravatar'
+  
 
 export const SimpleDiv = () => 
 	<div>
+		<Gravatar email="mbk@mbmgroup.dk" size={150} />
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!<br/>
 		Ad dolore dignissimos asperiores dicta facere optio quod commodi nam tempore recusandae. Rerum sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio aliquam!<br/>
 		Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae tempora quibusdam illum! Modi velit odio nam nulla unde amet odit pariatur at!<br/>
